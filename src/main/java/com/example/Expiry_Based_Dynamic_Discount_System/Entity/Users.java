41bfa5dc9945af1dda4ada01a4799e859812ed15
Tanishqvasani2906 <121330015+Tanishqvasani2906@users.noreply.github.com>
2025-03-05 16:04:25 +0530

    changes in the user entity

--- src/main/java/com/example/Expiry_Based_Dynamic_Discount_System/Entity/Users.java
+++ src/main/java/com/example/Expiry_Based_Dynamic_Discount_System/Entity/Users.java
@@ -10,46 +10,28 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-//@Data
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String user_id;
-    @Column(name = "username" , nullable = true, unique = true)
+
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
-    @Column(name = "password" , nullable = true , unique = true)
+
+    @Column(name = "password", nullable = false)
     private String password;
-    @Column(name = "first_name" , nullable = false)
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
-    @Column(name = "last_name" , nullable = true)
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    @Column(name = "email" , nullable = false , unique = true)
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
-    @Column(name = "phone" , nullable = true , unique = true)
-    private String phone;
-    @Column(name = "address")
-    private String address;
-    @Column(name = "gender")
-    private String gender;
 
     @Column(name = "temp_token")
     private String tempToken;
-
-    public LocalDateTime getTempTokenExpiry() {
-        return tempTokenExpiry;
-    }
-
-    public void setTempTokenExpiry(LocalDateTime tempTokenExpiry) {
-        this.tempTokenExpiry = tempTokenExpiry;
-    }
-
-    public String getTempToken() {
-        return tempToken;
-    }
-
-    public void setTempToken(String tempToken) {
-        this.tempToken = tempToken;
-    }
 
     @Column(name = "temp_token_expiry")
     private LocalDateTime tempTokenExpiry;
@@ -58,70 +40,7 @@ public class Users {
     @Column(name = "roles", nullable = false)
     private Role role = Role.USER;
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
+    // Getters and Setters
     public String getUser_id() {
         return user_id;
     }
@@ -138,6 +57,59 @@ public class Users {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTempToken() {
+        return tempToken;
+    }
+
+    public void setTempToken(String tempToken) {
+        this.tempToken = tempToken;
+    }
+
+    public LocalDateTime getTempTokenExpiry() {
+        return tempTokenExpiry;
+    }
+
+    public void setTempTokenExpiry(LocalDateTime tempTokenExpiry) {
+        this.tempTokenExpiry = tempTokenExpiry;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
